457300d96e34f7c043688f0be61ba4c917272e3b
vicki <surajbarik2003@gmail.com>
2024-12-06 23:41:08 +0530

    JUnit test

--- src/test/kotlin/com/example/bank/datasource/mock/MockBankDataSourceTest.kt
+++ src/test/kotlin/com/example/bank/datasource/mock/MockBankDataSourceTest.kt
@@ -1,7 +1,16 @@
 package com.example.bank.datasource.mock
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class MockBankDataSourceTest {
 
+    @Test
+    fun `should provide a collection of banks`() {
+        // given
+
+        // when
+
+        //then
+    }
 }
