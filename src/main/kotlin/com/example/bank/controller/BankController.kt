17bdddc53df90b28db45e85733819992d1a3e4d9
vicki <surajbarik2003@gmail.com>
2024-12-10 23:46:05 +0530

    web layer

---
+++ src/main/kotlin/com/example/bank/controller/BankController.kt
@@ -0,0 +1,15 @@
+package com.example.bank.controller
+
+import com.example.bank.model.Bank
+import com.example.bank.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+}
---
+++ src/test/kotlin/com/example/bank/controller/BankControllerTest.kt
@@ -0,0 +1,30 @@
+package com.example.bank.controller
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class BankControllerTest {
+
+    @Autowired // springBoot way of DI
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return all banks`() {
+        // when /then
+        mockMvc.get("/api/banks")
+            .andDo { println() }
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$[0].accountNumber") { value("9188") }
+            }
+    }
+}
